5a730c7ddbab08d7a05c4f3721fa05ef613ebae4
Brad <145044797+bradleylindquist@users.noreply.github.com>
2024-10-14 14:25:49 +0000

    added first python problem

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+# hello world, first exercise in HR python
+
+if __name__ == '__main__':
+    print("Hello, World!")
+
+This Python code snippet is using the if __name__ == '__main__': construct to 
+    control when a particular block of code is executed.
+
+# Here's how it works:
+
+# if __name__ == '__main__':
+
+# Every Python module has a special built-in attribute called __name__.
+
+# If the module is being run as the main program (i.e., it's the script being executed directly), 
+#     then the value of __name__ will be '__main__'.
+
+# This if condition checks whether the script is being executed directly 
+#     (as opposed to being imported as a module in another script).
+
+# print("Hello, World!")
+
+# If the script is run directly, it will print the message "Hello, World!" to the console.
+
+# Summary:
+# When you run this script, it will print "Hello, World!". However, if this code is 
+#     part of a module that is imported into another Python script, the print("Hello, World!") 
+#     will not be executed because __name__ will not be '__main__' in that case.
