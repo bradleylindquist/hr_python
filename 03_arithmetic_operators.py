abd6b4be4afafd47d8ef33313cb737eb33e1b45d
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-10-15 10:12:40 -0400

    added #3 arithmetic operators

---
+++ 03_arithmetic_operators.py
@@ -0,0 +1,12 @@
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+a = 3
+b = 5
+
+print(a + b, a - b, a * b, sep='\n')
+
+# print() has a possible sep argument that we can set to be a line break \n.
