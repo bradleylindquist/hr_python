5a1a3f6b16c0a3461191e11d1615d804c0094a29
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-10-15 13:19:18 -0400

    solved print function

---
+++ 07_print_function.py
@@ -0,0 +1,15 @@
+
+# Without using any string methods, try to print the following: 123...n
+
+# Note that "..." represents the consecutive values in between.
+
+n = 5
+
+count = '1'
+i = 2
+while i <= n:
+  count += str(i)
+  i+=1
+
+print(count)
+
