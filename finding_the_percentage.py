552c6ad02ac7af5d6db7d26c8cc622e2f4ebf6ef
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-11-02 09:33:34 -0400

    added solution

--- finding_the_percentage.py
+++ finding_the_percentage.py
@@ -6,3 +6,12 @@ student_marks = {'name': 'Malika', 'scores': [52, 56.5, 60]}
 answer = (sum(student_marks['scores']) / len(student_marks['scores']))
 print(round(float(answer), 2))
 
+
+    l1 = list(student_marks[query_name]) 
+
+    addition = sum(l1)
+
+    result = addition/len(l1)
+
+    print('%.2f'% result)
+
