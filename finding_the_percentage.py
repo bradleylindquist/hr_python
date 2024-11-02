61ecbc5c74be0c2b1dec8952eda205bb7b614673
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-11-02 09:27:14 -0400

    added finding the percentage

---
+++ finding_the_percentage.py
@@ -0,0 +1,8 @@
+student_marks = {'name': 'Malika', 'scores': [52, 56.5, 60]}
+
+
+# answer = ((sum(scores[name])) / (len(student_marks[name])))
+# answer = (round(answer, 2))
+answer = (sum(student_marks['scores']) / len(student_marks['scores']))
+print(round(float(answer), 2))
+
