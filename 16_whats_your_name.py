20bcf1b1b09fad72d6fd418e0279f6b1b9091a1d
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-11-13 15:18:14 -0500

    finished 16

---
+++ 16_whats_your_name.py
@@ -0,0 +1,8 @@
+# You are given the firstname and lastname of a person on two different lines. 
+# Your task is to read them and print the following: 
+#   Hello firstname lastname! You just delved into python.
+
+def print_full_name(first, last):
+    return('Hello ' + first + ' ' + last + '! You just delved into python.')
+
+print(print_full_name('Jean-Luc', 'Picard'))
