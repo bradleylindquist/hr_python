f23d71bce72fbca93a73eb566d119abd57758e90
Brad <145044797+bradleylindquist@users.noreply.github.com>
2024-11-13 17:10:27 +0000

    added solution for 11

--- 11_finding_the_percentage.py
+++ 11_finding_the_percentage.py
@@ -1,17 +1,14 @@
-student_marks = {'name': 'Malika', 'scores': [52, 56.5, 60]}
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+# Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+# dict = hash 
+
+student1_marks = {'name': 'Malika', 'scores': [52, 56.5, 60]}
+student2_marks = {'name': 'Krishna', 'scores': [67, 68, 69]}
+student3_marks = {'name': 'Arjun', 'scores': [70, 98, 63]}
+
+answer = (sum(student2_marks['scores']) / len(student2_marks['scores']))
+print(student2_marks['name'], round(float(answer), 2))
 
 
-# answer = ((sum(scores[name])) / (len(student_marks[name])))
-# answer = (round(answer, 2))
-answer = (sum(student_marks['scores']) / len(student_marks['scores']))
-print(round(float(answer), 2))
-
-
-    l1 = list(student_marks[query_name]) 
-
-    addition = sum(l1)
-
-    result = addition/len(l1)
-
-    print('%.2f'% result)
 
