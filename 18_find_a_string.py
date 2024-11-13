c5f5b9d593eaef86b10208cd1299d2c4d7f28000
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-11-13 15:32:36 -0500

    finished 18

--- 18_find_a_string.py
+++ 18_find_a_string.py
@@ -1,14 +1,16 @@
 # In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+
 # NOTE: String letters are case-sensitive.
+
 # Input Format
 # The first line of input contains the original string. The next line contains the substring.
-# Constraints
 
+# Constraints
+# length between 1 and 200
 # Each character in the string is an ascii character.
+
 # Output Format
 # Output the integer number indicating the total number of occurrences of the substring in the original string.
-
-# ----------------------brute force------------------
 
 string = 'ABCDCDC'
 sub_string = 'CDC'
